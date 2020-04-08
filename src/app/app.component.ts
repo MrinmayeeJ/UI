05d627fa6040700f1680616ba8baddf1be5d339a
Chat-bot-Erc <59439009+Chat-bot-Erc@users.noreply.github.com>
2020-04-08 12:31:46 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -208,3 +208,24 @@ export class AppComponent implements OnInit {
             })().catch(err => console.error(err));
 
     //<!--New code added till here for speech to text-->
+
+ var IdealTimeOut = 10; //10 seconds
+    var idleSecondsTimer = null;
+    var idleSecondsCounter = 0;
+    document.onclick = function () { idleSecondsCounter = 0; };
+    document.onmousemove = function () { idleSecondsCounter = 0; };
+    document.onkeypress = function () { idleSecondsCounter = 0; };
+    idleSecondsTimer = window.setInterval(CheckIdleTime, 1000);
+
+    function CheckIdleTime() {
+        idleSecondsCounter++;
+        var oPanel = document.getElementById("timeOut");
+        if (oPanel) {
+            oPanel.innerHTML = (IdealTimeOut - idleSecondsCounter);
+        }
+        if (idleSecondsCounter >= IdealTimeOut) {
+            window.clearInterval(idleSecondsTimer);
+            alert("Your Session has expired. Please login again.");
+            window.location = "https://www.aspsnippets.com/";
+        }
+    }
